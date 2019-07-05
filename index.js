43c16d0c1edb4801539265bd24edf9ae3ac22a19
Naresh Sarma <gnareshsarma@gmail.com>
2019-07-05 18:11:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function takeANumber(katzDeliLine, newName){
+  console.log
+  return 
+}
+  
+function nowServing(katzDeliLine) {
+  
+  return 
+}  
+  
+function currentLine(katzDeliLine){
+  
+  return
+}  
